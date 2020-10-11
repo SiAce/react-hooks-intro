276f2055fc9b2aefea47b074851ccc141f2e2d45
SiAce <siacespark@gmail.com>
2020-10-11 10:23:40 -0400

    add tailwind table

--- src/Cart.js
+++ src/Cart.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import CartItem from "./CartItem";
-import "./Cart.css"
+import "./Cart.css";
 
 function Cart({ initialItems }) {
-  const initialState = JSON.parse(window.localStorage.getItem('items'));
+  const initialState = JSON.parse(window.localStorage.getItem("items"));
   const [items, setItems] = useState(initialState || initialItems);
 
   useEffect(() => {
-    window.localStorage.setItem('items', JSON.stringify(items));
-  }, [items])
+    window.localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   const updateQty = (id, newQty) => {
     const newItems = items.map((item) => {
@@ -28,11 +28,43 @@ function Cart({ initialItems }) {
   return (
     <div className="Cart">
       <h1 className="Cart-title">Shopping Cart</h1>
-      <div className="Cart-items">
-        {items.map((item) => (
-          <CartItem key={item.id} updateQty={updateQty} {...item} />
-        ))}
+      {/*
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+*/}
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Name/Price
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Quantity
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Status
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Total
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50" />
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                    {items.map((item) => (
+                      <CartItem key={item.id} updateQty={updateQty} {...item} />
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
+
       <h2 className="Cart-total">Grand Total: ${grandTotal}</h2>
     </div>
   );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./CartItem.css"
+import "./CartItem.css";
 
 function CartItem({ id, name, price, qty, updateQty }) {
   const addOne = () => {
@@ -11,16 +11,68 @@ function CartItem({ id, name, price, qty, updateQty }) {
   };
 
   return (
-    <div className="CartItem">
-      <div>{name}</div>
-      <div>${price}</div>
-      <div>
-        <button onClick={subtractOne} disabled={qty < 1}>-</button>
-        {qty}
-        <button onClick={addOne}>+</button>
-      </div>
-      <div>Totol: ${price * qty}</div>
-    </div>
+    <tr>
+      <td className="px-6 py-4 whitespace-no-wrap">
+        <div className="flex items-center">
+          <div className="flex-shrink-0 h-10 w-10">
+            <img
+              className="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+              alt
+            />
+          </div>
+          <div className="ml-4">
+            <div className="text-sm leading-5 font-medium text-gray-900">
+              {name}
+            </div>
+            <div className="text-sm leading-5 text-gray-700">${price}</div>
+          </div>
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap">
+        <div className="text-sm leading-5 text-gray-900">
+          <button
+            className={`bg-blue-500 ${
+              qty >= 1 ? "hover:bg-blue-700" : "opacity-50 cursor-not-allowed"
+            } text-white font-bold py-0 px-2 rounded-full`}
+            onClick={subtractOne}
+            disabled={qty < 1}
+          >
+            -
+          </button>
+          <span className="text-sm leading-5 font-medium text-gray-900 px-4">
+            {qty}
+          </span>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-2 rounded-full"
+            onClick={addOne}
+          >
+            +
+          </button>
+        </div>
+        <div className="text-sm leading-5 text-gray-500">Optimization</div>
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap">
+        {qty > 0 ? (
+          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+            Active
+          </span>
+        ) : (
+          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+            Inactive
+          </span>
+        )}
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-700">
+        Totol: ${price * qty}
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+          Edit
+        </a>
+      </td>
+    </tr>
   );
 }
 
